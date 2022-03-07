5bed3ba8ee94b6d7cd4c3610cca018c06bccd304
miaojiacheng <101097489+miaojiacheng@users.noreply.github.com>
2022-03-07 20:51:00 +0800

    replace on github web

--- src/main/java/com/cxxy/git/GitTest.java
+++ src/main/java/com/cxxy/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git master test1!");
         System.out.println("hello git hot fix test1!");
         System.out.println("hello git push test!");
+        System.out.println("replace on github web!");
     }
 }
