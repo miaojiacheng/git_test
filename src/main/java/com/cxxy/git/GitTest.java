c810c6de039da660216eff4f9f06c0384b84b35e
miaojiacheng <1139807581@qq.com>
2022-03-07 20:46:10 +0800

    git test push test

--- src/main/java/com/cxxy/git/GitTest.java
+++ src/main/java/com/cxxy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git hot fix!");
         System.out.println("hello git master test1!");
         System.out.println("hello git hot fix test1!");
+        System.out.println("hello git push test!");
     }
 }
